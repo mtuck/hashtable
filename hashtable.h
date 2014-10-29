03927c9b4576312e9e0ac716399fa92ab543751b
Tyler Yasaka <tyleryasaka@users.noreply.github.com>
2014-10-29 15:40:46 -0500

    Templated

--- hashtable.h
+++ hashtable.h
@@ -1,22 +1,19 @@
-
+template <class T>
 class HashTable{
 Public:
 	HashTable();
-	HashTable(int size);
-	HashTable(const HashTable& n);
-	HashTable& operator =(const HashTable& rhs);
-	bool insert(int data);  
-	bool remove(int data);
-	bool search(int data) const;
-	int howMany() const;
+	HashTable(T size);
+	HashTable(const HashTable<T>& n);
+	HashTable& operator =(const HashTable<T>& rhs);
+	bool insert(T data);
+	bool remove(T data);
+	bool search(T data) const;
+	T howMany() const;
 	bool isFull() const;
 	bool isEmpty() const;
 	void clearTable();
 	void pirntTable(ostream& os) const;
 	~HashTable();
 Private:
-	int table[]; 
+	T table[]; 
 };
-
-
-
